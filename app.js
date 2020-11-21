70ded971849a8326ca1c87500854dc2a47106358
blaze83media <48297710+blaze83media@users.noreply.github.com>
2020-11-21 02:51:58 +0000

    changes_ new

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello People, this is a simple test!'));
+app.get('/', (req, res) => res.send('Some people are still out of job'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
+
+
+Dear sir/ma,
+
+Please find attached letters from me and my supervisor with reasons why my PhD programme has exceeded 36 months and is now 48 months. 
+
+Best regards,
+Blessing
